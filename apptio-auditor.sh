e0ed2ecac8fa9167abfcd9be077728a8a6223bf2
Chris Busch <cbusch@pivotal.io>
2017-09-12 09:26:34 -0500

    change scopes

--- apptio-auditor.sh
+++ apptio-auditor.sh
@@ -4,7 +4,7 @@ set -ex
 # audit user information
 AUDIT_USER="apptio-pcf-auditor"
 AUDIT_PWD="Appt10intX17"
-AUDIT_EMAIL="apptiopcfauditor@mastercard.com"
+AUDIT_EMAIL="apptiopcfauditor@company.com"
 
 # set target environment in which to create users
 #uaac target uaa.system.<DOMAIN.COM> --skip-ssl-validation
@@ -13,9 +13,11 @@ uaac target uaa.system.mypcf.net --skip-ssl-validation
 # Note: insert token after '-s' from Elastic Runtime tile -> Credentials tab -> UAA / Admin Client Credentials
 #uaac token client get admin -s <UAA ADMIN CLIENT PASSWORD>
 uaac token client get admin -s Jt5YRvF8VQWH2laqo_W159gsX--KveZ8
+#uaac token client get usage_service -s U_AIZXItMXurjHvMT0WIl52X2365WyqZ
 
 # create audit user
 uaac user add $AUDIT_USER -p $AUDIT_PWD --emails $AUDIT_EMAIL
-#uaac member add cloud_controller.admin_read_only $AUDIT_USER
 uaac member add cloud_controller.admin $AUDIT_USER
-
+#uaac member add cloud_controller.global_auditor $AUDIT_USER
+#uaac member add usage_service.audit $AUDIT_USER
+#uaac member add cloud_controller.admin_read_only $AUDIT_USER
